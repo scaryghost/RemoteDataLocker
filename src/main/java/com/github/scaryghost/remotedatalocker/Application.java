605415c606a6edb4b816e35b2db7e5da36f6f390
etsai <etsai2@gmail.com>
2016-02-11 01:30:40 -0800

    Use Jetty as the HTTP server underneath

--- src/main/java/com/github/scaryghost/remotedatalocker/Application.java
+++ src/main/java/com/github/scaryghost/remotedatalocker/Application.java
@@ -1,7 +1,21 @@
 package com.github.scaryghost.remotedatalocker;
 
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.main.Main;
+
 public class Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        final Main camelMain= new Main();
+
+        camelMain.enableHangupSupport();
+        camelMain.addRouteBuilder(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("jetty:http://0.0.0.0:8000").process((exchange) -> {
+                    exchange.getOut().setBody("Remote Data Locker on port 8000");
+                }).end();
+            }
+        });
+        camelMain.run();
     }
 }
