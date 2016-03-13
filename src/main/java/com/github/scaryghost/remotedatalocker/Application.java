0ace619abb3972ba6a8d2a3255ce4da6da2f3f65
etsai <etsai2@gmail.com>
2016-03-12 16:08:00 -0800

    Replaced Jetty with NanoHttpd
    Don't need all the Jetty features so use web server with smaller memory usage.  Switching reduced memory consumption by 50MB.

--- src/main/java/com/github/scaryghost/remotedatalocker/Application.java
+++ src/main/java/com/github/scaryghost/remotedatalocker/Application.java
@@ -1,81 +1,28 @@
 package com.github.scaryghost.remotedatalocker;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.main.Main;
-import org.apache.camel.main.MainListenerSupport;
-import org.apache.camel.main.MainSupport;
+import fi.iki.elonen.NanoHTTPD;
+import org.apache.commons.cli.ParseException;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.*;
 
-public class Application {
+/**
+ * Created by etsai on 3/12/2016.
+ */
+public class Application extends NanoHTTPD {
     private static final String HEADER_ACTION= "action", ACTION_GET= "get", ACTION_SAVE= "save";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws ParseException, ClassNotFoundException, SQLException, IOException {
         final ApplicationSettings settings= new ApplicationSettings(args);
 
         Class.forName(settings.dbDriver());
         final Connection dbConn= DriverManager.getConnection(settings.dbUrl());
         initDataBase(dbConn);
 
-        final Main camelMain= new Main();
+        new Application(settings.hostname(), settings.port(), dbConn);
 
-        camelMain.enableHangupSupport();
-        camelMain.addMainListener(new MainListenerSupport() {
-            @Override
-            public void beforeStop(MainSupport main) {
-                try {
-                    dbConn.close();
-                } catch (SQLException ignored) {
-                }
-            }
-        });
-        camelMain.addRouteBuilder(new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                from(String.format("jetty:http://%s:%d", settings.hostname(), settings.port()))
-                    .choice()
-                        .when(header(HEADER_ACTION).isEqualTo(ACTION_GET))
-                        .doTry()
-                        .process((exchange) -> {
-                            Message msg = exchange.getIn();
-
-                            PreparedStatement getState = dbConn.prepareStatement("select value from data where key=?");
-                            getState.setString(1, msg.getHeader("key", String.class));
-
-                            ResultSet result = getState.executeQuery();
-                            if (result.next()) {
-                                exchange.getOut().setBody(result.getString("value"));
-                            } else {
-                                exchange.getOut().setBody("");
-                                exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 204);
-                            }
-                        })
-                        .doCatch(SQLException.class).setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500)).endChoice()
-                        .when(header(HEADER_ACTION).isEqualTo(ACTION_SAVE))
-                        .doTry()
-                        .process((exchange) -> {
-                            Message msg = exchange.getIn();
-
-                            PreparedStatement insertOrIgnore = dbConn.prepareStatement("insert or ignore into data (key, value) values (?, ?)");
-                            insertOrIgnore.setString(1, msg.getHeader("key", String.class));
-                            insertOrIgnore.setString(2, "0");
-                            insertOrIgnore.executeUpdate();
-
-                            PreparedStatement updateData = dbConn.prepareStatement("update data set value=? where key=?");
-                            updateData.setString(1, msg.getHeader("value", String.class));
-                            updateData.setString(2, msg.getHeader("key", String.class));
-                            updateData.executeUpdate();
-
-                            dbConn.commit();
-                        })
-                        .doCatch(SQLException.class).setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500)).endChoice()
-                        .otherwise().to("mock:other")
-                        .end();
-            }
-        });
-        camelMain.run();
     }
 
     private static void initDataBase(final Connection conn) throws SQLException {
@@ -86,5 +33,72 @@ public class Application {
         stmt.executeUpdate("CREATE TABLE IF NOT EXISTS data (key text NOT NULL, value text NOT NULL, PRIMARY KEY (key));");
 
         conn.commit();
+    }
+
+    private final Connection dbConn;
+
+    public Application(String hostname, int port, Connection dbConn) throws IOException {
+        super(hostname, port);
+        this.dbConn= dbConn;
+
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        try {
+            session.parseBody(null);
+        } catch (IOException | ResponseException e) {
+            StringWriter sw= new StringWriter();
+            PrintWriter pw= new PrintWriter(sw);
+            e.printStackTrace(pw);
+
+            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, sw.toString());
+        }
+        String action= session.getParms().get(HEADER_ACTION);
+
+        switch(action) {
+            case ACTION_GET:
+                try {
+                    final PreparedStatement getData = dbConn.prepareStatement("select value from data where key=?");
+                    getData.setString(1, session.getParms().get("key"));
+
+                    ResultSet result = getData.executeQuery();
+                    if (result.next()) {
+                        return newFixedLengthResponse(Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, result.getString("value"));
+                    } else {
+                        return newFixedLengthResponse(Response.Status.NO_CONTENT, NanoHTTPD.MIME_PLAINTEXT, "");
+                    }
+                } catch (SQLException e) {
+                    StringWriter sw= new StringWriter();
+                    PrintWriter pw= new PrintWriter(sw);
+                    e.printStackTrace(pw);
+
+                    return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, sw.toString());
+                }
+            case ACTION_SAVE:
+                try {
+                    final PreparedStatement insertOrIgnore = dbConn.prepareStatement("insert or ignore into data (key, value) values (?, ?)");
+                    insertOrIgnore.setString(1, session.getParms().get("key"));
+                    insertOrIgnore.setString(2, "0");
+                    insertOrIgnore.executeUpdate();
+
+                    final PreparedStatement updateData = dbConn.prepareStatement("update data set value=? where key=?");
+                    updateData.setString(1, session.getParms().get("value"));
+                    updateData.setString(2, session.getParms().get("key"));
+                    updateData.executeUpdate();
+
+                    dbConn.commit();
+                    return newFixedLengthResponse(Response.Status.NO_CONTENT, NanoHTTPD.MIME_PLAINTEXT, "");
+                } catch (SQLException e) {
+                    StringWriter sw= new StringWriter();
+                    PrintWriter pw= new PrintWriter(sw);
+                    e.printStackTrace(pw);
+
+                    return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, sw.toString());
+                }
+            default:
+                return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, NanoHTTPD.MIME_PLAINTEXT, "Unrecognized action: " + action);
+        }
     }
 }
